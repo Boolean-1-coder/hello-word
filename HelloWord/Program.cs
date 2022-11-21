4df629070e3e096e11b98e4cd3a0272459112a64
bryan <lucchetta.work@gmail.com>
2022-11-21 11:51:46 +0100

    Aggiungere i file di progetto.

---
+++ HelloWord/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ciao come va?");
+
+var risposta = Console.ReadLine();
+
+Console.WriteLine("La risposta inserita dall'utente è: " + risposta);
